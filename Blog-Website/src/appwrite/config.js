61a0c568f29efa0f3f6b31385a59a4f122591984
guru <guruyewandge@gmail.com>
2025-07-21 07:59:24 +0530

    create services for file uploading etc

---
+++ Blog-Website/src/appwrite/config.js
@@ -0,0 +1,138 @@
+import conf from "../conf/conf";
+import { Client,ID,Databases,Storage,Query} from "appwrite";
+
+
+export class Service{
+
+    client = new Client();
+    databases;
+    bucket;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases= new Databases(this.client);
+        this.bucket= new Storage(this.client)
+    }
+
+    async createPost({title, slug,content,featuredImage,status,userID}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userID
+                }
+            )
+        } catch (error) {
+            console.log("appwrite service :: createPost",error);
+        }
+    }
+
+    async updatePost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+             console.log("appwrite service :: updatePost",error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+            return true
+        } catch (error) {
+             console.log("appwrite service :: deletePost",error);
+             return false
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("appwrite service :: getPost",error);
+             return false
+        }
+    }
+
+    //now i want all posts which are active so i used query here
+    //for query we need to make indexs in databses like we made keys as status
+
+    async getposts(queries = [Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+             console.log("appwrite service :: getPosts",error);
+             return false
+        }
+    }
+
+    //file upload
+
+    //many people do midtakes here we have to give blob of file not name   
+    
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("appwrite service :: uploadFile",error);
+             return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("appwrite service :: deleteFile",error);
+             return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service();
+
+export default service;
